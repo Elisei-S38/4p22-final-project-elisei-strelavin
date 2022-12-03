678ad17ce5cf93ecff831b41a32c80ff765374e4
Elisei Strelavin <spectrum388@gmail.com>
2022-12-03 21:24:21 +0300

    add FP

--- src/Pages/FavoritesFilmPage/FavoritesFilmPage.jsx
+++ src/Pages/FavoritesFilmPage/FavoritesFilmPage.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 const FavoritesFilmPage = () => {
   return (
     <div className='box'>
-      <h1 className='title'>Страница Избранного<br/>в разработке...</h1>
+      <h1 className='title'>Страница "Избранного"<br/>находится в разработке.<br/>Приносим свои извинения...</h1>
     </div>
   );
 };
